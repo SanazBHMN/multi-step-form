ad4cb758f2aa4959dc2cad6639c308a3f2f15e1e
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-02-24 14:52:22 +0330

    Implement plan form

---
+++ src/components/PlanForm.tsx
@@ -0,0 +1,14 @@
+export const PlanForm = () => {
+  return (
+    <>
+      <label htmlFor="">arcade</label>
+      <input type="checkbox" />
+
+      <label htmlFor="">advanced</label>
+      <input type="checkbox" />
+
+      <label htmlFor="">pro</label>
+      <input type="checkbox" />
+    </>
+  );
+};
