fb6f5d1dcc2790576e950636840d90389a266aa0
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-02-24 16:41:30 +0330

    Display steps responsively

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -5,12 +5,12 @@ interface StepProps {
 
 export const Step = ({ stepNumber, stepTitle }: StepProps) => {
   return (
-    <li className="flex items-start justify-start gap-3">
-      <div className="flex h-8 w-8 items-center justify-center rounded-full border">
+    <li className={`flex items-start justify-start gap-3 text-sm font-bold`}>
+      <div className="flex h-8 w-8 items-center justify-center rounded-full border text-white">
         {stepNumber}
       </div>
       {stepTitle && (
-        <div className="flex flex-col">
+        <div className="hidden flex-col sm:flex ">
           <p>STEP {stepNumber}</p>
           <p className="uppercase">{stepTitle}</p>
         </div>
--- src/components/StepsList.tsx
+++ src/components/StepsList.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from "react";
 // components
 import { Step } from "./Step";
+// images
+import mobileBackground from "../assets/images/bg-sidebar-mobile.svg";
+import desktopBackground from "../assets/images/bg-sidebar-desktop.svg";
 
 interface StepsListProps {
   steps: ReactElement[];
@@ -8,16 +11,26 @@ interface StepsListProps {
 
 export const StepsList = ({ steps }: StepsListProps) => {
   const stepTitles = ["your info", "select plan", "add-ons", "summary"];
-  //   console.log(steps);
+
   return (
-    <ol className="flex flex-col justify-start gap-6">
-      {steps.map((_, index) => (
-        <Step
-          key={index}
-          stepNumber={index + 1}
-          stepTitle={stepTitles[index]}
-        />
-      ))}
-    </ol>
+    <div className="relative">
+      <img
+        src={mobileBackground}
+        className="block h-auto w-full sm:hidden"
+        alt=""
+      />
+      <img src={desktopBackground} className="hidden sm:block" alt="" />
+      <ol
+        className={`absolute top-[50%] left-[50%] flex -translate-1/2 justify-center gap-4 sm:top-36 sm:left-24 sm:flex-col`}
+      >
+        {steps.map((_, index) => (
+          <Step
+            key={index}
+            stepNumber={index + 1}
+            stepTitle={stepTitles[index]}
+          />
+        ))}
+      </ol>
+    </div>
   );
 };
