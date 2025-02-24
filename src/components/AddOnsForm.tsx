c4d9bb1b10c43a3bd27cd96e8313ffde537e1c5b
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-02-24 14:52:41 +0330

    Implement add-ons form

---
+++ src/components/AddOnsForm.tsx
@@ -0,0 +1,14 @@
+export const AddOnsForm = () => {
+  return (
+    <>
+      <label htmlFor="">online services</label>
+      <input type="checkbox" />
+
+      <label htmlFor="">larger storage</label>
+      <input type="checkbox" />
+
+      <label htmlFor="">customizable storage</label>
+      <input type="checkbox" />
+    </>
+  );
+};
